1f82c56692e05e5c0f00fb531d496c61bb86c9a3
Xiaozhou Pu <Patrick_Pu@vagvlan506.5193.wireless-pennnet.upenn.edu>
2016-11-20 14:56:22 -0500

    Routes are set

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
